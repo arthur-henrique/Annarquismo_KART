627b7521c5e7d49d5a6230f97ad3288078e86fa2
arthur-henrique <arthur.henrique.h@outlook.com>
2023-04-10 12:12:49 -0300

    PlayerRespawn Beginnings

--- GOKART/Assets/NOSSO JOGO/Scripts/Items/FallCatch.cs
+++ GOKART/Assets/NOSSO JOGO/Scripts/Items/FallCatch.cs
@@ -1,18 +1,65 @@
+using KartGame.KartSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FallCatch : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody rb;
+    float tempoDeStun;
+    [SerializeField] float maxTempStun;
+    bool stunado, stunadoOver;
+
+    public ArcadeKart kart;
+
+    public ArcadeKart.StatPowerup boostStats = new ArcadeKart.StatPowerup
     {
-        
+        MaxTime = 5
+    };
+    public UnityEvent onPowerupActivated;
+    private void Update()
+    {
+        if (stunado)
+        {
+            rb.constraints = RigidbodyConstraints.FreezePositionX;
+            rb.constraints = RigidbodyConstraints.FreezePositionZ;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+
+
+        rb = other.attachedRigidbody;
+
+        if (rb)
+        {
+
+            kart = rb.GetComponent<ArcadeKart>();
+            //var kart = rb.GetComponent<Rigidbody>();
+
+
+            if (kart)
+            {
+                stunado = true;
+                EventToCall();
+            }
+        }
     }
+
+    public void EventToCall()
+    {
+        StartCoroutine(TimeOver());
+    }
+    IEnumerator TimeOver()
+    {
+        yield return new WaitForSeconds(maxTempStun);
+        kart.ReSpawn();
+        stunado = false;
+        rb.constraints = RigidbodyConstraints.None;
+
+    }
+
 }
